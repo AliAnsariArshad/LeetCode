2109db8b87c7c61fddf872c11df6635419cb8810
arshad-ansari-sdc <arshad.ansari@smiledirectclub.com>
2022-03-08 21:27:58 +0530

    added doc string

--- Problems/Hard/plusOne.py
+++ Problems/Hard/plusOne.py
@@ -1,6 +1,18 @@
 class Solution:
+    """
+    You are given a large integer represented as an integer array digits,
+    where each digits[i] is the ith digit of the integer. The digits are ordered
+    from most significant to least significant in left-to-right order. The large
+    integer does not contain any leading 0's.
+    Increment the large integer by one and return the resulting array of digits.
+    """
 
     def plus_one(self, digits):
+        """
+
+        :param digits: accept digits array from user
+        :return: new digits array
+        """
         if digits[-1] < 9:
             num = digits[len(digits) - 1] + 1
             digits.pop()
